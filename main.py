9d4e07d3f2373c9ad9a9b70350f6440fd6ee797f
Praneet2003 <rajpraneet4@gmail.com>
2025-04-09 07:40:36 +0530

    Fixed a bug of export data button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 from modules.system_monitor import get_system_info, get_process_info
 from modules.data_handler import save_data, export_data
 from modules.ui_components import create_labels, create_process_table, create_buttons
